482388e3d9b9c99b9c408a4f641d9217be179aaa
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-04-20 20:19:27 -0500

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int percentInt = int.Parse(percent);
+
+        string letter;
+
+        if (percentInt >= 97)
+        {
+            letter = "A";
+        }
+        else if (percentInt >= 90 && percentInt <= 93)
+        {
+            letter = "A-";
+        }
+        else if (percentInt >= 87)
+        {
+            letter = "B+";
+        }
+        else if (percentInt >= 80 && percentInt <= 83)
+        {
+            letter = "B-";
+        }
+        else if (percentInt >= 77)
+        {
+            letter = "C+";
+        }
+          else if (percentInt >= 70 && percentInt <= 73)
+        {
+            letter = "C-";
+        }
+        else if (percentInt >= 67)
+        {
+            letter = "D+";
+        }
+          else if (percentInt >= 60 && percentInt <= 63)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}. ");
+
+        if (percentInt >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed! ");
+        }
+        else
+        {
+            Console.WriteLine("That's okay, you'll do better next time! ");
+        }
+
     }
 }
