4c14bf5a8d4f346ab96af91844f933c43ac9003c
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-05-06 19:14:27 -0500

    began writing journal program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices. \n1. Write \n2. Display \n3. Load \n4. Save \n5. Quit");
     }
 }
