5cd731a9bb25ed382ad8eebb0fca5cab4e96edaa
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-05-09 20:17:29 -0500

    while loop for selection menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,39 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Journal Program!");
-        Console.WriteLine("Please select one of the following choices. \n1. Write \n2. Display \n3. Load \n4. Save \n5. Quit");
-        Console.WriteLine("What would you like to do? (Enter a number 1-5: )");
-        string selection = Console.ReadLine();
+        string selection = "";
+      
+        while (selection != "5")
+        {
+            
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices. \n1. Write \n2. Display \n3. Load \n4. Save \n5. Quit");
+            Console.WriteLine("What would you like to do? (Enter a number 1-5: )");
+            selection = Console.ReadLine();
 
-        if (selection == "1") 
-        {
-            // code for Write entry
+            if (selection == "1") 
+            {
+                // code for Write entry
 
-        }
-        else if (selection == "2") 
-        {
-            // code for Display entries
+            }
+            else if (selection == "2") 
+            {
+                // code for Display entries
 
-        }
-        else if (selection == "3") 
-        {
-            // code for Load file
+            }
+            else if (selection == "3") 
+            {
+                // code for Load file
 
+            }
+            else if (selection == "4") 
+            {
+                // code for Save file
+            }
+            else if (selection == "5") 
+            {
+                // code for Quit
+            }
+            else 
+            {
+                // if user enters character other than 1-5
+                Console.WriteLine("Invalid Selection. Select an option number 1-5.");
+            }
         }
-        else if (selection =="4") 
-        {
-            // code for Save file
-        }
-        else if (selection == "5") 
-        {
-            // code for Quit
-        }
-        else 
-        {
-            Console.WriteLine("Invalid Selection");
-        }
-
 
     }
 }
