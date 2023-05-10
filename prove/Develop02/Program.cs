43b7137ba3180c339c70f96d3e97153a4cb291df
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-05-09 22:16:56 -0500

    generates a random prompt from list

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,43 +5,42 @@ class Program
     static void Main(string[] args)
     {
         string selection = "";
-      
-        while (selection != "5")
+        PromptGenerator random_prompt = new PromptGenerator();
+        
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices. \n1. Write \n2. Display \n3. Load \n4. Save \n5. Quit");
+        Console.Write("What would you like to do? (Enter a number 1-5: ) ");
+        selection = Console.ReadLine();
+
+        if (selection == "1") 
         {
-            
-            Console.WriteLine("Welcome to the Journal Program!");
-            Console.WriteLine("Please select one of the following choices. \n1. Write \n2. Display \n3. Load \n4. Save \n5. Quit");
-            Console.WriteLine("What would you like to do? (Enter a number 1-5: )");
-            selection = Console.ReadLine();
+            // code for Write entry
+            random_prompt.GenerateRandomPrompt();
 
-            if (selection == "1") 
-            {
-                // code for Write entry
+        }
+        else if (selection == "2") 
+        {
+            // code for Display entries
 
-            }
-            else if (selection == "2") 
-            {
-                // code for Display entries
+        }
+        else if (selection == "3") 
+        {
+            // code for Load file
 
-            }
-            else if (selection == "3") 
-            {
-                // code for Load file
+        }
+        else if (selection == "4") 
+        {
+            // code for Save file
 
-            }
-            else if (selection == "4") 
-            {
-                // code for Save file
-            }
-            else if (selection == "5") 
-            {
-                // code for Quit
-            }
-            else 
-            {
-                // if user enters character other than 1-5
-                Console.WriteLine("Invalid Selection. Select an option number 1-5.");
-            }
+        }
+        else if (selection == "5")
+        {
+            // code for Quit (selection == "5")
+            Console.WriteLine("Thank you for using the Journal Program. Goodbye!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Entry. Select an option number 1-5.");
         }
 
     }
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,22 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    static void Main(string[] args)
-    {
-        List<string> prompts = new List<string>() {
-            "What is one thing I'm looking forward to?",
-            "What am I most grateful for today?", 
-            "How did I see the hand of the Lord in my life today?",
-            "What was the best part of my day?", 
-            "What did I learn today?", 
-            "What was the most challenging thing I faced today?"
-        };
+    public Random random = new Random();
+    public List<string> _prompts = new List<string>(){
+        "What is one thing I'm looking forward to?",
+        "What am I most grateful for today?", 
+        "How did I see the hand of the Lord in my life today?",
+        "What was the best part of my day?", 
+        "What did I learn today?", 
+        "What was the most challenging thing I faced today?"
+    };
 
-    foreach (string prompt in prompts)
+    public void GenerateRandomPrompt()
     {
-        Console.WriteLine(prompt);
+        int random_index = random.Next(_prompts.Count);
+        Console.WriteLine(_prompts[random_index]);
     }
-    }
+
+
     
 }
