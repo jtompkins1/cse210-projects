2d649fe7b09d1f2c08f16a87ea08a1828d8985de
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-05-23 22:38:07 -0500

    add comments

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,19 +8,20 @@ class Program
         // string renderedText;
         // bool isCompletelyHidden;
         
-        // string text = "Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.";
-        Reference r1 = new Reference();
-        Scripture s1 = new Scripture(r1);
-        Console.WriteLine(s1.GetScripture());
-        Console.WriteLine();
+        // Complex Reference
+        // Reference r1 = new Reference();
+        // Scripture s1 = new Scripture(r1);
+        // Console.WriteLine(s1.GetScripture());
 
-        Reference r2 = new Reference("John", 3, 16);
-        Scripture s2 = new Scripture(r2, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
-        Console.WriteLine(s2.GetScripture());
-        Console.WriteLine();
+        // Simple reference
+        // Reference r2 = new Reference("John", 3, 16);
+        // Scripture s2 = new Scripture(r2, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        // Console.WriteLine(s2.GetScripture());
 
-        Scripture s3 = new Scripture();
-        Console.WriteLine(s3.GetScripture());
+        // get default scripture reference and text
+        Scripture scripture = new Scripture();
+        Console.WriteLine(scripture.GetScripture());
+        Console.WriteLine();
 
 
 
