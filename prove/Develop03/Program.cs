ce0fe3ced7bfd3994524c18f8e0e8a46a32ae044
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-05-23 22:22:23 -0500

    correctly displays scripture with reference/text

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,11 +8,17 @@ class Program
         // string renderedText;
         // bool isCompletelyHidden;
         
-        string text = "Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.";
-        Reference r1 = new Reference("Matthew", 11, 28, 30);
+        // string text = "Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.";
+        Reference r1 = new Reference();
+        Scripture s1 = new Scripture(r1);
+        Console.WriteLine(s1.GetScripture());
+        Console.WriteLine();
 
-        Scripture scripture = new Scripture(r1, text);
-        Console.WriteLine(scripture.GetScripture());
+        Reference r2 = new Reference("John", 3, 16);
+        Scripture s2 = new Scripture(r2, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        Console.WriteLine(s2.GetScripture());
+        Console.WriteLine();
+
 
 
         // do
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -5,25 +5,26 @@ public class Scripture
     private string _scripture;
     private Reference _reference;
     private string _text;
-    public Scripture(string text, Reference reference)
+
+    public Scripture()
+    {
+        _reference = new Reference("Matthew", 11, 28, 30);
+        _text = "Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.";
+        SetScripture(_reference, _text);
+    }
+    public Scripture(Reference reference)
     {
         _reference = reference;
-        _text = text;
-
+        _text = "Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.";
+        SetScripture(reference, _text);
     }
+
     public Scripture(Reference reference, string text)
     {
         _reference = reference;
         _text = text;
+        SetScripture(reference, text);
     }
-    // public string GetText()
-    // {
-    //     return _text;
-    // }
-    // public void SetText(string text)
-    // {
-    //     _text = text;
-    // }
     public string GetScripture()
     {
         return _scripture;
@@ -32,11 +33,11 @@ public class Scripture
     {
         if (_reference.GetStartVerse() == _reference.GetEndVerse())
         {
-            _scripture = ($"{_reference.GetBook()} {_reference.GetChapter()}: {_reference.GetStartVerse()} {_text}");
+            _scripture = ($"{_reference.GetBook()} {_reference.GetChapter()}:{_reference.GetStartVerse()} {_text}");
         }
         else
         {
-            _scripture = ($"{_reference.GetBook()} {_reference.GetChapter()}: {_reference.GetStartVerse()}-{_reference.GetEndVerse()} {_text}");
+            _scripture = ($"{_reference.GetBook()} {_reference.GetChapter()}:{_reference.GetStartVerse()}-{_reference.GetEndVerse()} {_text}");
         }
         
     }
