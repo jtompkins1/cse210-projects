0b437c759e5e8dc87525c1fb29f50c937501fe78
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-05-23 21:10:47 -0500

    scripture

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,25 +1,27 @@
 using System;
-using System.Collections.Generic;
 
 public class Scripture
 {
     private string _scripture;
-    private List<Word> _words;
-    private string _reference;
-    private string _text = "Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.";
-
-    Reference r1 = new Reference();
-
-
-    public Scripture()
+    private Reference _reference;
+    private string _text;
+    public Scripture(string text)
     {
-        _reference = r1.GetReferenceString();
-        string[] _words = _text.Split(" ");
+        _reference = new Reference("Matthew", 11, 28, 30);
+        _text = "Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.";
     }
-    public string GetRenderedText()
+    public Scripture(string text, Reference _reference)
     {
-        string _text = String.Join(" ", _words);
-        return _scripture = ($"{_reference} {_text}");
+        _reference = new Reference();
+        _text = text;
+    }
+    public string GetText()
+    {
+        return _text;
+    }
+    public void SetText(string text)
+    {
+        _text = text;
     }
 
 
