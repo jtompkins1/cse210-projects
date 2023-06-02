7c3f8b4f502fa30b591339fc9c8900b63a2e4301
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-06-01 22:17:02 -0500

    menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string menuSelection = "";
+        List<string> menu = new List<string> {"Start Breathing Activity", "Start Reflection Activity", "Start Listing Activity", "Quit" };
+
+        Console.WriteLine("Menu Options:");
+
+        for (int i = 0; i < menu.Count; i++)
+        {
+            string menuItem = menu[i];
+            Console.WriteLine($" {i + 1}. {menuItem}");
+        }
+
+        Console.Write("Select an option from the menu (1-4): ");
+        menuSelection = Console.ReadLine();
+        
     }
 }
