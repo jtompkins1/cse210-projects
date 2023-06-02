2126640d9185eb9629a249a39ca8d0db98958bb6
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-06-01 22:48:00 -0500

    do while

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 
 class Program
 {
+
     static void Main(string[] args)
     {
         string menuSelection = "";
         List<string> menu = new List<string> {"Start Breathing Activity", "Start Reflection Activity", "Start Listing Activity", "Quit" };
 
-        Console.WriteLine("Menu Options:");
+        do{
+            Console.WriteLine("Menu Options:");
 
-        for (int i = 0; i < menu.Count; i++)
-        {
-            string menuItem = menu[i];
-            Console.WriteLine($" {i + 1}. {menuItem}");
-        }
+            for (int i = 0; i < menu.Count; i++)
+            {
+                string menuItem = menu[i];
+                Console.WriteLine($" {i + 1}. {menuItem}");
+            }
 
-        Console.Write("Select an option from the menu (1-4): ");
-        menuSelection = Console.ReadLine();
+            Console.Write("Select an option from the menu (1-4): ");
+            menuSelection = Console.ReadLine();
+            
+        }while (menuSelection != "4");
+
+
         
     }
 }
