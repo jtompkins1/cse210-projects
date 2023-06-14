d34d63ce2ed6dc6659a487a45396cb9fe3ecf46e
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2023-06-13 22:24:28 -0500

    learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+  
+        Square s1 = new Square("red",12);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("blue", 10, 18);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Yellow", 3);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}. ");
+        }
+
+
+
+
+
+
     }
 }
